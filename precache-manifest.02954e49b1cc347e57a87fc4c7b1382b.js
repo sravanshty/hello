ecdf5356253eff0df00258dec60003a86e515451
Sravan Kumar Shetty <sshetty@evoketechnologies.com>
2018-12-26 15:22:40 +0530

    Updates

---
+++ precache-manifest.02954e49b1cc347e57a87fc4c7b1382b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "734fb1b9a94fbb48dd25",
+    "url": "/hello/static/js/main.734fb1b9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/hello/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "734fb1b9a94fbb48dd25",
+    "url": "/hello/static/css/main.7b698d8d.chunk.css"
+  },
+  {
+    "revision": "be9eedf72aeb3a2af95271b6f2adeba5",
+    "url": "/hello/index.html"
+  }
+];
